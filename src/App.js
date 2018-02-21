7fc86dc80fec141f56a5ce99de1dbc7cd6440b4d
Robert Wilson <youmufan@gmail.com>
2018-02-20 21:07:08 -0500

    updated latitude and longitude input, and added change handlers for each.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat:0,
+      lon:0
+    }
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+  }
+
+  handleLatChange(e)
+  {
+    console.log('lat');
+    this.setState({
+      lat: +e.target.value
+    })
+  }
+
+  handleLonChange(e)
+  {
+    console.log('lon');
+    this.setState({
+      lon: +e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
@@ -9,9 +35,11 @@ class App extends Component {
         <p>Enter your latitude and longitude to get the weather!</p>
         <form>
           <label>Latitude:</label>
-          <input type="text" />
+          <input onChange={(e) => this.handleLatChange(e)}
+          placeholder="Enter Latitude" value={this.state.lat} type="text" />
           <label>Longitude:</label>
-          <input type="text" />
+          <input onChange={(e) => this.handleLonChange(e)}
+          placeholder="Enter Longitude" value={this.state.lon} type="text" />
           <button type="submit">Calculate Weather!</button>
         </form>
       </div>
