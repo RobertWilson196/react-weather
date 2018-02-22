ba3b193ae18dd98e5c8a8ce5f4aa390fbf44f001
Robert Wilson <youmufan@gmail.com>
2018-02-21 20:25:55 -0500

    made the request of weather data dynamic based on latitude and longitude

--- server/index.js
+++ server/index.js
@@ -13,15 +13,14 @@ const port = process.env.PORT || 5000;
 //                 : means variable ( changing )
 serverApp.get('/forecast/:lat,:lon', function(request, response)
 {
-    const url = `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`;
+    const { lat, lon } = request.params;
+    const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
     axios.get(url)
-        .then(res => {
-            response.json(res.data);
-        })
+        .then(res => {response.status(200).json(res.data)})
         .catch(err => {
-            response.json({
+            response.status(500).json({
                 msg: "your stuff is broked."
-            })
+            });
         });
 
 }); 
